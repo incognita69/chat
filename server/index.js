137b72144f78f8166e294abe412d56be3924ad8c
incógnita69 <angel.osorio3008@gmail.com>
2025-06-25 00:41:36 -0600

    Actualización del servidor con corrección en base de datos y puerto

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const io = new Server(server,{
     connectionStateRecovery: {} 
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 || 1000 ;
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
